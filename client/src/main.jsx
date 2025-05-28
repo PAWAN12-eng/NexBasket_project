cbd15b187ec36e86bdb85458975f0a391c37839c
PAWAN12-eng <jk5357367@gmail.com>
2025-05-28 22:31:04 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './Store/Store'; 
 import { RouterProvider } from 'react-router-dom';
-import routerpage from './route/Index';
+import routerpage from './route/index';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
